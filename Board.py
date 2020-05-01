25030d37553ea7826eb48c52510bcdcdd467b76e
Josue Zorrilla <josuezorrilla23@gmail.com>
2020-04-30 20:15:15 -0400

    add some methods to class

--- Board.py
+++ Board.py
@@ -1,8 +1,8 @@
-
+from Player import Player
+from Pawn import Pawn
+from Piece import Piece
 class GameBoard:
-    def __init__(self,player1,player2):
-        self.player1 = player1
-        self.player2 = player2
+    def __init__(self):
         self.board =  [["",3*" "+"A", 5*" "+"B", 5*" "+"C", 5*" "+"D",5*" "+"E",5*" "+"F",5*" "+"G",5*" "+"H"],
          ["8","[    ]", "[    ]", "[    ]", "[    ]","[    ]", "[    ]", "[    ]", "[    ]"],
          ["7","[    ]", "[    ]", "[    ]", "[    ]","[    ]", "[    ]", "[    ]", "[    ]"],
@@ -13,44 +13,51 @@ class GameBoard:
          ["2","[    ]", "[    ]", "[    ]", "[    ]","[    ]", "[    ]", "[    ]", "[    ]"],
          ["1","[    ]", "[    ]", "[    ]", "[    ]","[    ]", "[    ]", "[    ]", "[    ]"]]
 
-    def drawBoard(self):
+    def draw_board(self):
         lineToPrint = ""
         for line in self.board:
             for cell in line:
                 lineToPrint = lineToPrint + " " + cell
             print(lineToPrint)
             lineToPrint = ""
+    
+    def set_player(self,player):
+        name1 = input("Name of the first player: ")
 
-juego = GameBoard("Josue","Mario")
+        self.player1 = Player(name1,"black",[])
+        name2 = input("Name of the Second player: ")
+        self.player2 = Player(name2,"white",[])
+    
+    def draw_piece(self,piece:Piece):
+        self.board[piece.positionX][piece.positionY] = piece.image
 
-juego.drawBoard()
-  
+    def set_pieces(self):
+        pieces = []
+        for i in range(1,9):
+            pawn = Pawn("Pawn","Black",2,i,"[ \u2659  ]")
+            pieces.append(pawn)
+        return pieces
+        
+
+
     
 
+juego = GameBoard()
+
+juego.draw_board()
+
+#fila,columna = getCordinate("A8")
+juego.draw_board()
+#p = Pawn("Black","Peon",3,5)
+#juego.draw_piece(p)
+juego.set_pieces()
+pcs = juego.set_pieces()
+for p in pcs:
+    juego.draw_piece(p)
+
+juego.draw_board()
 
 
 
-    
-
-# #getCordinate("A8")
-# def getCordinate(cell):
-#     fila = ord(cell[0:1]) - 64
-#     columna = 9 - int(cell[1:])
-#     return fila,columna
-
-# def MoveCell(fila,columna,board):
-#     board[fila][columna] = "[ \u2659  ]"
-#     return board
-
-# printBoard(board)    
-# fila,columna = getCordinate("A8")
-# printBoard(MoveCell(fila,columna,board))
-# #print(printCell("H1"))
-
-# def MovePawn(cordinate,board):
-#     if ord(cordinate[0:1]) > 72 or int(cordinate[1:]) > 8:
-#         print("Movimiento fuera de rango.")
-#     else:
-#         return MoveCell(9-int(cordinate[1:]),ord(cordinate[0:1])-64,board)
 
 # printBoard(MovePawn("B2",board))
